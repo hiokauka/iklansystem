886578f210381086224dbafdefbfe307348ffb8f
Rauf Hazim <raufhazim07@gmail.com>
2025-11-11 14:49:48 +0800

    update anon key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-
+const supabaseUrl = 'https://gfeenhqwwnwdtqivffdg.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmZWVuaHF3d253ZHRxaXZmZmRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0MjEyMTUsImV4cCI6MjA3Nzk5NzIxNX0.0QcHKGEH87DiWcpuBFjaa3556wuu4Knd_D7P9rm56VI'
 export const supabase = createClient(supabaseUrl, supabaseKey)
